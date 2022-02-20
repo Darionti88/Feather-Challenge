463202fb3c57b79a24ff374e7ded03bb606f59fb
Darionti88 <darionti@gmail.com>
2022-02-20 20:24:32 -0300

    Mutation Working without refetch

--- frontend/src/components/Table/TableRow.tsx
+++ frontend/src/components/Table/TableRow.tsx
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import { AllPolicy } from "../../interfaces/allPolicies.interface";
 import editSvg from "../../assets/icons/edit.svg";
 import saveSvg from "../../assets/icons/save.svg";
+import closeSvg from "../../assets/icons/close.svg";
 import { Input } from "@popsure/dirty-swan";
+import { useMutation } from "@apollo/client";
+import { EDIT_POLICY } from "../../graphql/mutations";
+import { ALL_POLICIES, SORTED_POLICIES } from "../../graphql/querys";
 
 const TableRow = (policy: AllPolicy) => {
   const [edit, setEdit] = useState({
@@ -14,16 +18,33 @@ const TableRow = (policy: AllPolicy) => {
     endDate: false,
     createdAt: false,
   });
+  const [newValue, setNewValue] = useState({
+    provider: "",
+    insuranceType: "",
+    status: "",
+  });
 
-  const handleSave = (field: string) => {
+  const [editPolicy] = useMutation(EDIT_POLICY, {
+    refetchQueries: [
+      { query: SORTED_POLICIES, variables: { orderBy: { provider: "asc" } } },
+    ],
+  });
+
+  const handleSave = (
+    field: string,
+    fieldValue: string,
+    policyNumber: string
+  ) => {
     setEdit({ ...edit, [field]: false });
+    const newFieldAndValue = { [field]: fieldValue };
+    editPolicy({ variables: { policyNumber, edit: newFieldAndValue } });
   };
 
   return (
     <tr className='bg-gray-50'>
       <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
         <a href='#' className='font-bold text-blue-500 hover:underline'>
-          {`${policy?.customer?.firstName} ${policy?.customer?.lastName}`}
+          {`${policy.customer.firstName} ${policy.customer.lastName}`}
         </a>
       </td>
       <td className='p-3 text-sm text-gray-700 flex flex-row justify-between whitespace-nowrap'>
@@ -40,14 +61,31 @@ const TableRow = (policy: AllPolicy) => {
           </>
         ) : (
           <>
-            <Input className='border-text border-2 border-opacity-20 rounded-md' />
-            <img
-              src={saveSvg}
-              height={17}
-              width={17}
-              alt='edit-icon'
-              onClick={() => handleSave("provider")}
+            <Input
+              className='border-text border-2 border-opacity-20 rounded-md'
+              value={newValue.provider}
+              onChange={(e) =>
+                setNewValue({ ...newValue, provider: e.target.value })
+              }
             />
+            <div className='flex flex-row space-x-3'>
+              <img
+                src={saveSvg}
+                height={17}
+                width={17}
+                alt='edit-icon'
+                onClick={() =>
+                  handleSave("provider", newValue.provider, policy.policyNumber)
+                }
+              />
+              <img
+                src={closeSvg}
+                height={17}
+                width={17}
+                alt='edit-icon'
+                onClick={() => setEdit({ ...edit, provider: false })}
+              />
+            </div>
           </>
         )}
       </td>
